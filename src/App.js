5d38e65c88f051c8f6a985eb20d7bd65af49c86b
ccozens <ccozens@gmail.com>
2022-08-30 09:25:27 +0100

    reordered imports as build failed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import TagManager from 'react-gtm-module'
-
-const tagManagerArgs = {
-    gtmId: 'GTM-NQZD4LP'
-}
-
-TagManager.initialize(tagManagerArgs)
-
 import './App.css';
 import { Navbar } from './components/navbar';
 import { Transcribe } from './components/transcribe';
 import { ReverseComplementDNA } from './components/reverse-complement';
 import { Translate } from './components/translate';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-NQZD4LP'
+}
+
+TagManager.initialize(tagManagerArgs);
 
 function App() {
   return (
