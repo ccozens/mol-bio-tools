a2f3e1455b21d7f4641e1fe8e50c995e560121bc
ccozens <ccozens@gmail.com>
2023-03-02 14:45:37 +0000

    testing adding coverage flag to github workflow

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,23 +1,18 @@
 const config = {
-   
-    collectCoverageFrom: [
-      "./src/components/**"
-    ],
-    coverageThreshold: {
-      "global": {
-        "functions": 80,
-        "lines": 80,
-        "statements": -10
-      }
+  collectCoverageFrom: ['./src/components/**', './src/scripts/**'],
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+      statements: -10,
     },
-    transformIgnorePatterns: [
-      "<rootDir>/node_modules/(?!d3-shape | d3-path)"
-    ],
-    transform: {
-        "^.+\\.[t|j]sx?$": "babel-jest"
-      },
-  }
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!d3-shape | d3-path)',
+  ],
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+};
 
-  module.exports = config;
-
-  
+module.exports = config;
