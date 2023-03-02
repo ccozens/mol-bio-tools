fffe0b94023290e63602bf5ac92892b7d3dffd77
ccozens <ccozens@gmail.com>
2023-03-02 13:21:23 +0000

    removed primer-check.js, updated checkProteinOneLetterInput.js and added checkProteinOneLetterInput.test.js

--- src/scripts/checkProteinOneLetterInput.js
+++ src/scripts/checkProteinOneLetterInput.js
@@ -1,6 +1,8 @@
-// find locations of non-proteinInput characters
+const naturalAminoAcidsRegex = new RegExp(/[^ARNDCEQGHILKMFPSTWYV]/gi);
+
+// find locations of non-protein input characters
 const findMatches = (proteinInput) => {
-  const matches = proteinInput.matchAll(/[^ARNDCEQGHILKMFPSTWYV]/gi);
+  const matches = proteinInput.matchAll(naturalAminoAcidsRegex);
   const indexes = [];
   for (const match of matches) {
     indexes.push(match.index + 1);
@@ -9,24 +11,26 @@ const findMatches = (proteinInput) => {
 };
 
 export const checkProteinOneLetterInput = (proteinInput) => {
-
-  if (proteinInput === '') {
+  // ensure uppercase
+  const proteinUpper = proteinInput.toUpperCase();
+  //remove any line breaks
+  const protein = proteinUpper.replace(/[\r\n]/gm, '');
+  
+  if (protein === '') {
     return '';
   } else {
-    //remove any line breaks
-    const protein = proteinInput.replace(/[\r\n]/gm, '');
     // if protein is in single letter format, create array and check for non-natural AA input
     let proteinArray = [];
     // convert to array
     proteinArray = Array.from(protein);
     // check array is entirely composed of natural AAs (ARNDCEQGHILKMFPSTWYV)
     for (let resi of proteinArray) {
-      if (resi.match(/[^ARNDCEQGHILKMFPSTWYV]/))
+      if (resi.match(naturalAminoAcidsRegex))
         return (
           `Non-amino acid character entered, please enter only 20 natural residues.  Non-protein characters at positions: ${findMatches(proteinInput)}`) 
     }
 
-    // return unchanged proteinInput if for loop exits successfully
+    // return uppercase proteinInput if for loop exits successfully
     return protein;
   }
   
--- src/scripts/dnaAnalysis.ts
+++ src/scripts/dnaAnalysis.ts
@@ -1,8 +1,5 @@
-import { countNucleotides } from "./count-nt";
-import { ntMW } from './lookupTables'
-
-// lookuptable
-
+import { countNucleotides } from './count-nt';
+import { ntMW } from './lookupTables';
 
 // approx MW
 export const approxRnaMw = (dna) => {
@@ -19,38 +16,34 @@ export const approxDsDnaMw = (dna) => {
 
 // exact MW
 export const exactSsDnaMw = (dna: string) => {
-    let dnaArray = Array.from(dna).map(char => 'd' + char); // map appends d to each nt so DNA nts lookedup
-    // calc MW
-      let dnaMW = 0;
-      dnaArray.forEach((pos) => {
-        dnaMW += ntMW[pos];
-          });
-      const phosphate = ntMW.monophosphate;
-      let finalDnaMW = dnaMW+phosphate;
-      
-      return (
-        finalDnaMW
-      );
-    };
+  let dnaArray = Array.from(dna).map((char) => 'd' + char); // map appends d to each nt so DNA nts lookedup
+  // calc MW
+  let dnaMW = 0;
+  dnaArray.forEach((pos) => {
+    dnaMW += ntMW[pos];
+  });
+  const phosphate = ntMW.monophosphate;
+  let finalDnaMW = dnaMW + phosphate;
 
-    export const exactDsDnaMw = (dna) => {
-        let dnaArray = Array.from(dna).map(char => 'd' + char); // map appends d to each nt so DNA nts lookedup
-    // calc MW
-      let dnaMW = 0;
-      dnaArray.forEach((pos) => {
-        dnaMW += ntMW[pos];
-      });
-      const phosphate = ntMW.monophosphate;
-      let finalDnaMW = (dnaMW+phosphate)*2;
-      
-      return (
-        finalDnaMW
-      );
-    }
+  return finalDnaMW;
+};
 
-    export const gcRatio = (dna) => {
-        let ntCounts = countNucleotides(dna);
-        let gc = ntCounts.C + ntCounts.G;
-        let at = ntCounts.A + ntCounts.T;
-        return (gc/ (gc+at)).toFixed(3);
-    }
+export const exactDsDnaMw = (dna) => {
+  let dnaArray = Array.from(dna).map((char) => 'd' + char); // map appends d to each nt so DNA nts lookedup
+  // calc MW
+  let dnaMW = 0;
+  dnaArray.forEach((pos) => {
+    dnaMW += ntMW[pos];
+  });
+  const phosphate = ntMW.monophosphate;
+  let finalDnaMW = (dnaMW + phosphate) * 2;
+
+  return finalDnaMW;
+};
+
+export const gcRatio = (dna) => {
+  let ntCounts = countNucleotides(dna);
+  let gc = ntCounts.C + ntCounts.G;
+  let at = ntCounts.A + ntCounts.T;
+  return (gc / (gc + at)).toFixed(3);
+};
